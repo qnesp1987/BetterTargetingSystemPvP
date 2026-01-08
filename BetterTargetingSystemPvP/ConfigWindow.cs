18d03e32d42d64c93978e9add1e32dd0366c6a05
qnesp1987 <99622366+qnesp1987@users.noreply.github.com>
2026-01-08 04:48:59 -0500

    s
    s

--- BetterTargetingSystemPvP/ConfigWindow.cs
+++ BetterTargetingSystemPvP/ConfigWindow.cs
@@ -5,220 +5,38 @@ using System;
 using System.Numerics;
 using BetterTargetingSystem.Keybinds;
 
-namespace BetterTargetingSystem.Windows
+namespace BetterTargetingSystemPvP
 {
     public class ConfigWindow : Window, IDisposable
     {
         private readonly Plugin Plugin;
-        private Configuration Configuration;
-        public Keybind CurrentKeys { get; private set; }
-
-        private bool ModifyingKeybindTTK = false;
-        private bool ModifyingKeybindCTK = false;
-        private bool ModifyingKeybindLHTK = false;
-        private bool ModifyingKeybindBAOETK = false;
+        private readonly Configuration Configuration;
 
         public ConfigWindow(Plugin plugin) : base(
             "Better Targeting System",
-            // Cast necessary: Window constructor expects Dalamud.Interface.Windowing.ImGuiWindowFlags
-            (Dalamud.Interface.Windowing.ImGuiWindowFlags)(ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
+            ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
         {
             this.Size = new Vector2(185, 270);
-            // Cast necessary: SizeCondition expects Dalamud.Interface.Windowing.ImGuiCond
-            this.SizeCondition = (Dalamud.Interface.Windowing.ImGuiCond)ImGuiCond.Appearing; 
+            this.SizeCondition = ImGuiCond.Appearing;
 
             this.Plugin = plugin;
             this.Configuration = plugin.Configuration;
-
-            this.CurrentKeys = new Keybind();
         }
 
-        public void Dispose() { }
-
-        public override void Draw() {
-            if (ImGui.BeginTabBar("BTSConfigTabs", ImGuiTabBarFlags.None))
-            {
-                if (ImGui.BeginTabItem("Keybinds"))
-                {
-                    KeybindsConfig();
-                    ImGui.EndTabItem();
-                }
-                ImGui.EndTabBar();
-            }
-        }
-
-        private void KeybindsConfig()
+        public override void Draw()
         {
-            if (this.ModifyingKeybindTTK || this.ModifyingKeybindCTK || this.ModifyingKeybindLHTK || this.ModifyingKeybindBAOETK)
-                this.CurrentKeys = GetKeys();
+            if (!IsOpen) return;
 
-            var tabTargetKeybind = this.ModifyingKeybindTTK
-                ? this.CurrentKeys.ToString()
-                : (this.Configuration.TabTargetKeybind.Key != null ? this.Configuration.TabTargetKeybind.ToString() : "None");
-            var closestTargetKeybind = this.ModifyingKeybindCTK
-                ? this.CurrentKeys.ToString()
-                : (this.Configuration.ClosestTargetKeybind.Key != null ? this.Configuration.ClosestTargetKeybind.ToString() : "None");
-            var lowestHealthTargetKeybind = this.ModifyingKeybindLHTK
-                ? this.CurrentKeys.ToString()
-                : (this.Configuration.LowestHealthTargetKeybind.Key != null ? this.Configuration.LowestHealthTargetKeybind.ToString() : "None");
-            var bestAOETargetKeybind = this.ModifyingKeybindBAOETK
-                ? this.CurrentKeys.ToString()
-                : (this.Configuration.BestAOETargetKeybind.Key != null ? this.Configuration.BestAOETargetKeybind.ToString() : "None");
+            ImGui.TextUnformatted("Better Targeting System - Configuration");
+            ImGui.Separator();
 
-            ImGui.PushItemWidth(170);
-            ImGui.Text("\n[Cycle Targets]");
-            ImGui.InputText($"##ttk_Keybind", ref tabTargetKeybind, 200, ImGuiInputTextFlags.ReadOnly);
-            if (ImGui.IsItemActive())
-            {
-                ImGui.SetTooltip("Use Backspace to remove keybind");
-                this.ModifyingKeybindTTK = true;
-                if (this.CurrentKeys.Key != null && (this.CurrentKeys.Key != VirtualKey.TAB || this.CurrentKeys.AltModifier == false))
-                {
-                    this.Configuration.TabTargetKeybind = this.CurrentKeys;
-                    this.Configuration.Save();
-                    UnfocusInput();
-                }
-                else if (ImGui.IsKeyPressed(ImGuiKey.Backspace))
-                {
-                    this.Configuration.TabTargetKeybind = new Keybind();
-                    this.Configuration.Save();
-                    UnfocusInput();
-                }
-            }
-            else
-            {
-                this.ModifyingKeybindTTK = false;
-            }
-
-            ImGui.Text("\n");
-
-            ImGui.Text("[Closest Target]");
-            ImGui.InputText($"##ctk_Keybind", ref closestTargetKeybind, 200, ImGuiInputTextFlags.ReadOnly);
-            if (ImGui.IsItemActive())
-            {
-                ImGui.SetTooltip("Use Backspace to remove keybind");
-                this.ModifyingKeybindCTK = true;
-                if (this.CurrentKeys.Key != null && (this.CurrentKeys.Key != VirtualKey.TAB || this.CurrentKeys.AltModifier == false))
-                {
-                    this.Configuration.ClosestTargetKeybind = this.CurrentKeys;
-                    this.Configuration.Save();
-                    UnfocusInput();
-                }
-                else if (ImGui.IsKeyPressed(ImGuiKey.Backspace))
-                {
-                    this.Configuration.ClosestTargetKeybind = new Keybind();
-                    this.Configuration.Save();
-                    UnfocusInput();
-                }
-            }
-            else
-            {
-                this.ModifyingKeybindCTK = false;
-            }
-
-            ImGui.Text("\n");
-
-            ImGui.Text("[Lowest Health Target]");
-            ImGui.InputText($"##lhtk_Keybind", ref lowestHealthTargetKeybind, 200, ImGuiInputTextFlags.ReadOnly);
-            if (ImGui.IsItemActive())
-            {
-                ImGui.SetTooltip("Use Backspace to remove keybind");
-                this.ModifyingKeybindLHTK = true;
-                if (this.CurrentKeys.Key != null && (this.CurrentKeys.Key != VirtualKey.TAB || this.CurrentKeys.AltModifier == false))
-                {
-                    this.Configuration.LowestHealthTargetKeybind = this.CurrentKeys;
-                    this.Configuration.Save();
-                    UnfocusInput();
-                }
-                else if (ImGui.IsKeyPressed(ImGuiKey.Backspace))
-                {
-                    this.Configuration.LowestHealthTargetKeybind = new Keybind();
-                    this.Configuration.Save();
-                    UnfocusInput();
-                }
-            }
-            else
-            {
-                this.ModifyingKeybindLHTK = false;
-            }
-
-            ImGui.Text("\n");
-
-            ImGui.Text("[Best AOE Target]");
-            ImGui.InputText($"##baoetk_Keybind", ref bestAOETargetKeybind, 200, ImGuiInputTextFlags.ReadOnly);
-            if (ImGui.IsItemActive())
-            {
-                ImGui.SetTooltip("Use Backspace to remove keybind");
-                this.ModifyingKeybindBAOETK = true;
-                if (this.CurrentKeys.Key != null && (this.CurrentKeys.Key != VirtualKey.TAB || this.CurrentKeys.AltModifier == false))
-                {
-                    this.Configuration.BestAOETargetKeybind = this.CurrentKeys;
-                    this.Configuration.Save();
-                    UnfocusInput();
-                }
-                else if (ImGui.IsKeyPressed(ImGuiKey.Backspace))
-                {
-                    this.Configuration.BestAOETargetKeybind = new Keybind();
-                    this.Configuration.Save();
-                    UnfocusInput();
-                }
-            }
-            else
-            {
-                this.ModifyingKeybindBAOETK = false;
-            }
-
-            ImGui.NewLine();
-            if (ImGui.Button("Reset settings to defaults", new Vector2(170,25)))
-            {
-                Configuration.Cone1Angle = 140;
-                Configuration.Cone1Distance = 5;
-                Configuration.Cone2Enabled = true;
-                Configuration.Cone2Angle = 90;
-                Configuration.Cone2Distance = 15;
-                Configuration.Cone3Enabled = true;
-                Configuration.Cone3Angle = 50;
-                Configuration.Cone3Distance = 40;
-                Configuration.CloseTargetsCircleEnabled = true;
-                Configuration.CloseTargetsCircleRadius = 5;
-                Configuration.Save();
-            }
-
-            Plugin.DebugMode.Draw();
+            // Sample controls - keep minimal to avoid introducing new dependencies
+            ImGui.Text("Settings go here.");
         }
 
-        private void UnfocusInput()
+        public void Dispose()
         {
-            this.ModifyingKeybindTTK = false;
-            this.ModifyingKeybindCTK = false;
-            this.ModifyingKeybindLHTK = false;
-            this.ModifyingKeybindBAOETK = false;
-            this.CurrentKeys = new Keybind();
-            ImGui.SetWindowFocus(null); 
-            ImGui.SetWindowFocus(); 
-        }
-
-        private Keybind GetKeys()
-        {
-            VirtualKey? key = null;
-            var io = ImGui.GetIO();
-            var ctrl = io.KeyCtrl;
-            var shift = io.KeyShift;
-            var alt = io.KeyAlt;
-
-            if (ImGui.IsKeyPressed(ImGuiKey.Tab))
-                return new Keybind(VirtualKey.TAB, ctrl, shift, alt);
-
-            Keybind.GetKeyboardState();
-            foreach (var k in Keybind.SupportedKeys)
-            {
-                if (Keybind.IsKeyDown((int) k))
-                {
-                    key = k;
-                    break;
-                }
-            }
-            return new Keybind(key, ctrl, shift, alt);
+            // Cleanup if needed
         }
     }
 }
